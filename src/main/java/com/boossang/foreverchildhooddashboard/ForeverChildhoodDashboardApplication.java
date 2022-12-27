4090f0edeb38c4eadf2626e8800de24b20fee770
부상훈 <sboo@gmarket.com>
2022-12-27 20:57:15 +0900

    First Commit

---
+++ src/main/java/com/boossang/foreverchildhooddashboard/ForeverChildhoodDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.boossang.foreverchildhooddashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForeverChildhoodDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForeverChildhoodDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boossang/foreverchildhooddashboard/ForeverChildhoodDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boossang.foreverchildhooddashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeverChildhoodDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
